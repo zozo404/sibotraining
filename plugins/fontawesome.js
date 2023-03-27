4dfe3c34e50ce2475bb1cd0beddde8e55b6c7175
zozo404 <enzocuny66@gmail.com>
2023-03-27 17:01:09 +0200

    ✅ | header v1

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -2,7 +2,7 @@
 import Vue from 'vue'
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { fas, faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
+import { fas, faBars, faXmark, faCaretDown } from '@fortawesome/free-solid-svg-icons'
 
-library.add(fas, faBars, faXmark)
+library.add(fas, faBars, faXmark, faCaretDown)
 dom.watch()
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,36 @@
 module.exports = {
-    darkMode: 'class', // or 'media' or 'class'
-    theme: {
-      extend: {
-        colors: {
-          olive: {
-            50: '#5D750F'
-          },
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        olive: {
+          50: '#5D750F'
+        },
+        gold: {
+          25: '#F4E9CC',
+          50: '#E4C87F'
+        },
+        black: {
+          50: '#101010'
+        },
+        velour: {
+          50: '#560039'
         }
       }
-    },
-    variants: {
-      extend: {}
-    },
-    plugins: [],
-    content: [
-      'components/**/*.{vue,js}',
-      'layouts/**/*.vue',
-      'pages/**/*.vue',
-      'static/**/*.html',
-      'composables/**/*.{js,ts}',
-      'plugins/**/*.{js,ts}',
-      'App.{js,ts,vue}',
-      'app.{js,ts,vue}'
-    ]
-  }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [],
+  content: [
+    'components/**/*.{vue,js}',
+    'layouts/**/*.vue',
+    'pages/**/*.vue',
+    'static/**/*.html',
+    'composables/**/*.{js,ts}',
+    'plugins/**/*.{js,ts}',
+    'App.{js,ts,vue}',
+    'app.{js,ts,vue}'
+  ]
+}
